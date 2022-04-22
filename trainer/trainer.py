5c1dfafc2d400768f36bbd022d6f963c3fdd4556
Zhao <ziz531@login01.o2.rc.hms.harvard.edu>
2022-04-22 18:02:28 -0400

    fix

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -78,7 +78,8 @@ def model_train(model, temporal_contr_model, model_optimizer, temp_cont_optimize
         if training_mode == "ts_sd": # note, in config files, just use gaussian noise, this is to match the denoising
                                      # task used in the ts_sd paper
             base_signal = aug1
-            denoised_signal = 0 # ??
+            noisy_signal = aug2
+            denoised_signal = temporal_contr_model(noisy_signal)
             print('hit ts_sd case in train model')
             exit(1)
         else:
