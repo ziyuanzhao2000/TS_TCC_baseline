Minor fix in normalization

data_preprocessing/emg/preprocess.py
@@ -34,7 +34,7 @@ for data_file_name, label in zip(data_file_names, labels):
         y.append(label)
 
 X = np.stack(tuple(X))
-X = (X - signal_min) / (signal_max - signal_min)
+X = (X - 0) / (signal_max - signal_min)
 y = np.array(y)
 
 # 6-2-2 split

data_preprocessing/physionet2017/preprocess.py
@@ -51,7 +51,7 @@ def split_signal(X, y):
     X_len = [arr.shape[0] // window_len for arr in X]
     X = [arr[:arr_len * window_len].reshape((arr_len, window_len)) for (arr, arr_len) in zip(X, X_len)]
     X = np.vstack(tuple(X))
-    X = (X - signal_min) / (signal_max - signal_min) # normalize
+    X = (X - 0) / (signal_max - signal_min) # normalize
     X = np.expand_dims(X, axis=1) # equivalently, unsqueeze
     y_replicated = [[label] * n for (n, label) in tqdm(zip(X_len, y))]
     y = np.array([el for sublst in y_replicated for el in sublst])